3c829bf39e79ff2dfd7ba93b4f8911e57e54b8d6
iagoa <iagoantunes.f@gmail.com>
2022-01-28 17:03:32 -0300

    Alterando Imagens Tipo-Pokemon

--- Pokedex/Pokedex/Servicos/Model/ListaTipos.cs
+++ Pokedex/Pokedex/Servicos/Model/ListaTipos.cs
@@ -8,9 +8,9 @@ namespace Pokedex.Servicos.Model
     {
         public static List<Categorias> ListaCategorias = new List<Categorias>()
         {
-            new Categorias{Nome="Fire",Cor="#FFA152",Imagem="IconFire",CorFundo="#FFA756"},
-            new Categorias{Nome="Grass",Cor="#35C04A",Imagem="IconGrass",CorFundo="#8BBE8A"},
-            new Categorias{Nome="Poison",Cor="#B97FC9",Imagem="IconPoison"}
+            new Categorias{Nome="Fire",Cor="#FFA152",Imagem="iconFireSVG",CorFundo="#FFA756"},
+            new Categorias{Nome="Grass",Cor="#35C04A",Imagem="IconGrassSVG",CorFundo="#8BBE8A"},
+            new Categorias{Nome="Poison",Cor="#B97FC9",Imagem="IconPoisonSVG",CorFundo="#A566C7"}
         };
         public static List<Pokemon> ListaPokemons = new List<Pokemon>()
        {
