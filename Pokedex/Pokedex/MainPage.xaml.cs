b0b16ab47a832fc4b2a0303af70a24388087984a
iagoa <iagoantunes.f@gmail.com>
2022-01-27 22:24:30 -0300

    Padrao MVVM

--- Pokedex/Pokedex/MainPage.xaml.cs
+++ Pokedex/Pokedex/MainPage.xaml.cs
@@ -12,35 +12,13 @@ namespace Pokedex
 {
     public partial class MainPage : ContentPage
     {
-        public ObservableCollection<Pokemon> ListaP { get; set; }
-        public List<string> listaPo { get; set; }
+        
         public MainPage()
         {
             InitializeComponent();
-
-            listaPo = new List< string>
-            {
-                "bulbasaur","ivysaur","venusaur","charmander"
-            };
-
-            //Pokemon listaPok = Servicos.ServicoConexao.ReceberPokemon();
-            //var a = listaPok.results;
-
-            List<Pokemon> lista = Servicos.ServicoConexao.ReceberPokemon(listaPo);
-            atribuirValores(lista);
-            Iteste.ItemsSource = lista;
-            BindingContext = this;
+            BindingContext = new ViewModel.HomeViewModel();
+           
         }
-        public void atribuirValores(List<Pokemon> lista)
-        {
-            lista[0].imagem = "BulbasaurP2";
-            lista[1].imagem = "Ivysaur";
-            lista[2].imagem = "Venusaur";
-            lista[3].imagem = "Charmander";
-            lista[0].corFundo = "#8BBE8A";
-            lista[1].corFundo = "#8BBE8A";
-            lista[2].corFundo = "#8BBE8A";
-            lista[3].corFundo = "#FFA756";
-        }
+   
     }
 }
---
+++ Pokedex/Pokedex/ViewModel/HomeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Pokedex.Servicos.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Pokedex.ViewModel
+{
+    public class HomeViewModel
+    {
+        public ObservableCollection<Pokemon> ListaP { get; set; }
+        public List<string> listaNomes { get; set; }
+        public List<Pokemon> listaItens { get; set; }
+        public HomeViewModel()
+        {
+            listaNomes = new List<string>
+            {
+                "bulbasaur","ivysaur","venusaur","charmander"
+            };
+
+            //Pokemon listaPok = Servicos.ServicoConexao.ReceberPokemon();
+            //var a = listaPok.results;
+
+            listaItens = Servicos.ServicoConexao.ReceberPokemon(listaNomes);
+            atribuirValores(listaItens);
+        }
+        public void atribuirValores(List<Pokemon> lista)
+        {
+            lista[0].imagem = "BulbasaurP2";
+            lista[1].imagem = "Ivysaur";
+            lista[2].imagem = "Venusaur";
+            lista[3].imagem = "Charmander";
+
+            lista[0].corFundo = "#8BBE8A";
+            lista[1].corFundo = "#8BBE8A";
+            lista[2].corFundo = "#8BBE8A";
+            lista[3].corFundo = "#FFA756";
+        }
+    }
+}
